8e910fc49a50c3a790115061ed69880e660a9883
震宇 <1417003485@qq.com>
2020-12-09 08:40:10 +0800

    sdsds

---
+++ src/view/list/index.vue
@@ -0,0 +1,21 @@
+<template>
+     <div>
+         列表
+         <br>
+         <button @click="toDetail">详情</button>
+     </div>
+</template>
+
+<script>
+export default {
+    name:"detail",
+    methods:{
+        toDetail(){
+            this.$router.push('/detail')
+        }
+    },
+    created:{
+        
+    }
+}
+</script>
